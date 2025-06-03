d70ce2d72ee19d21105badeb32fb2f6869d7ffbf
turniejs4 <fikushah@gmail.com>
2025-06-03 12:52:05 +0200

    1

--- components/ui/input.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react"
-import { cn } from "../../lib/utils"
-
-export const Input = React.forwardRef(({ className, type, ...props }, ref) => (
-  <input type={type} className={cn("flex h-9 w-full rounded-md border bg-white px-3 py-1 text-sm text-black shadow-sm", className)} ref={ref} {...props} />
-))
-Input.displayName = "Input"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   content: [
-    "./app/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}"
+    "./app/**/*.{js,jsx}"
   ],
   theme: {
     extend: {},
